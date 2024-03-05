adfcc37a7a5ac8ede9e31756bc725895c82a1980
student-28 <nandhagopal57144@gmail.com>
2024-03-05 17:27:02 +0530

    New File

---
+++ ex.java
@@ -0,0 +1,7 @@
+public class ex{
+  public static void main(String[] args) {
+    int a=5;
+    int b=10;
+    System.out.println(a+b);
+  }
+}
--- odd.java
+++
@@ -1,7 +0,0 @@
-public class odd{
-  public static void main(String[] args) {
-    int a=100;
-    int b=10;
-    System.out.println(a);
-  }
-}
